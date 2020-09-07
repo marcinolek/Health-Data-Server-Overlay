a5478bc06f1769330423636c81d226be1483fcf5
Marcin Olek <marcin@spotio.com>
2020-09-08 00:07:12 +0200

    make the layout responsive for phones, change styling (dark mode)

--- public/socketClient.js
+++ public/socketClient.js
@@ -2,69 +2,73 @@ let config;
 let heartbeatTimeout;
 
 function connect() {
-    let socket = new WebSocket('ws://' + config.websocketIp + ':' + config.websocketPort);
+  let socket = new WebSocket(
+    "ws://" + config.websocketIp + ":" + config.websocketPort
+  );
 
-    let statusDisplay = null;
-    let dataDisplay = null;
+  let statusDisplay = null;
+  let dataDisplay = null;
 
-    let heartRateText = null;
-    let caloriesText = null;
+  let heartRateText = null;
+  let caloriesText = null;
 
-    socket.onopen = function (event) {
-        console.log('Connected to server on port: ' + config.websocketPort);
+  socket.onopen = function (event) {
+    console.log("Connected to server on port: " + config.websocketPort);
 
-        statusDisplay = document.getElementById('statusDisplay');
-        dataDisplay = document.getElementById('dataDisplay');
+    statusDisplay = document.getElementById("statusDisplay");
+    dataDisplay = document.getElementById("dataDisplay");
 
-        heartRateText = document.getElementById('heartRate');
-        caloriesText = document.getElementById('calories');
+    heartRateText = document.getElementById("heartRate");
+    caloriesText = document.getElementById("calories");
 
-        document.getElementById('hrImage').src = typeof config.hrImage === 'undefined' ? 'hrImage.png' : config.hrImage;
-        document.getElementById('calImage').src = typeof config.calImage === 'undefined' ? 'calImage.png' : config.calImage;
-    };
+    document.getElementById("hrImage").src =
+      typeof config.hrImage === "undefined" ? "hrImage.png" : config.hrImage;
+    document.getElementById("calImage").src =
+      typeof config.calImage === "undefined" ? "calImage.png" : config.calImage;
+  };
 
-    socket.onclose = function (event) {
-        console.log('Disconnected from server');
-        setTimeout(function () {
-            connect();
-        }, 1000);
-    };
+  socket.onclose = function (event) {
+    console.log("Disconnected from server");
+    setTimeout(function () {
+      connect();
+    }, 1000);
+  };
 
-    socket.onerror = function (event) {
-        console.log('WebSocket error');
-        // TODO: Does this mean the socket is disconnected?
-        // setTimeout(function() {
-        //     connect();
-        // }, 1000);
-    };
+  socket.onerror = function (event) {
+    console.log("WebSocket error");
+    // TODO: Does this mean the socket is disconnected?
+    // setTimeout(function() {
+    //     connect();
+    // }, 1000);
+  };
 
-    socket.onmessage = function (event) {
-        console.log(event.data);
+  socket.onmessage = function (event) {
+    console.log(event.data);
 
-        statusDisplay.style.display = 'none';
-        dataDisplay.style.display = 'block';
-        clearTimeout(heartbeatTimeout);
-        heartbeatTimeout = setTimeout(function () {
-            console.log('Disconnected from watch');
-            statusDisplay.style.display = 'block';
-            dataDisplay.style.display = 'none';
-        }, 60000); // 60 seconds
+    statusDisplay.style.display = "none";
+    dataDisplay.style.display = "block";
+    clearTimeout(heartbeatTimeout);
+    heartbeatTimeout = setTimeout(function () {
+      console.log("Disconnected from watch");
+      statusDisplay.style.display = "block";
+      dataDisplay.style.display = "none";
+    }, 60000); // 60 seconds
 
-        let data = event.data.split(':');
+    let data = event.data.split(":");
 
-        if (data[0] === 'heartRate') {
-            heartRateText.textContent = data[1];
-        }
+    if (data[0] === "heartRate") {
+      heartRateText.textContent = data[1];
+    }
 
-        if (data[0] === 'calories') {
-            caloriesText.textContent = data[1];
-        }
-    };
+    if (data[0] === "calories") {
+      caloriesText.textContent = data[1];
+    }
+  };
 }
 
 // Request the config from the server
 let xmlHttp = new XMLHttpRequest();
-xmlHttp.open('GET', '/config', false); // false for synchronous request
+xmlHttp.open("GET", "/config", false); // false for synchronous request
 xmlHttp.send(null);
 config = JSON.parse(xmlHttp.responseText);
 connect();
